80e937ff831e2b165962287237cf437b5cd2da4e
Miller Lai <miller.lai@gmail.com>
2020-06-18 08:49:01 +0000

    Y Added VPC module

--- vpc.tf
+++ vpc.tf
@@ -2,5 +2,11 @@
 module "vpc" {
   source  = "app.terraform.io/miller-training/vpc/aws"
   version = "2.39.0"
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "dedicated"
+
+  tags = {
+    Name = "main"
+  }
 }
 
